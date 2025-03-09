339df2d7ee16c8e0724fa69839300e81a32df118
bharathidasans <bharathi_dasan84@yahoo.co.in>
2025-03-09 18:31:48 +0530

    added credit card component

--- src/Components/Pagenotfound.js
+++ src/Components/Pagenotfound.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router";
+
 const Pagenotfound = () => {
-    const error = useRouteError();
+    const error = useRouteError;
+    console.log("error.status == ",error);
     return (
         error.status === "404" ?
         <div className="container text-center img-width">
             <img src="page-not-found.png" alt="not found"/>
             <h4>No Page Found </h4> Go to <a href='/'>Home</a>
-        </div> :""
+        </div> :<div><h2>Error</h2></div>
     );
 }
 
